d9b04863db12fe2d5cbd529571022c0e997e0f8d
Private JS <privateJS@Josephs-MacBook-Pro.local>
2022-01-25 18:44:19 +0100

    adapted to LP without functions f and g and fixed car bug

--- SmartChargeLP.py
+++ SmartChargeLP.py
@@ -4,58 +4,79 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 
-#------Car---------
-car_soc = 0.5                                # Read from TeslaAPI
+#------Functions---------
+def g_function(x, y):
+    if x <= y:
+        return 0
+    else:
+        return -(x-y)
+
+def f_function(x, y):
+    if x >= y:
+        return y
+    else:
+        return x
+
+#*************Power Consumers*************
+
+#------Car
+car_soc = 0.7                                # Read from TeslaAPI
 car_battery_capacity = 75                    # Capacity: 75kwh
-required_min_car_soc = 0.2 * car_battery_capacity
+car_soc_min = 0.2
+car_soc_min_kwh = 0
+car_discharge_level = (1 - car_soc) * car_battery_capacity
 
-#------Global Vars---------
-avg_home_consumption = 14.3                # Read from previous data  
+if car_soc < car_soc_min:
+    car_soc_min_kwh = (car_soc_min - car_soc) * car_battery_capacity
 
-if car_soc > 0.2:
-    required_min_car_soc = 0
+#------Household
+avg_home_consumption = 14.3                # Derived from previous data, maybe connect to TeslaAPI
 
-min_total_power_required = avg_home_consumption + required_min_car_soc          
-max_total_power_required = avg_home_consumption + ((1 - car_soc) * car_battery_capacity)
+#------Total Power
+min_total_power_required = avg_home_consumption + car_soc_min_kwh          
+max_total_power_required = avg_home_consumption + car_discharge_level
 
-#-------Powerwall----------
-powerwalls_soc = 0.8                       # Read from TeslaAPI
-if powerwalls_soc < 0.2:
-    powerwalls_soc = 0.2
-powerwall_power = powerwalls_soc * 24.4
+#*************Power Producers*************
+#----Grid Power
+grid_power = 1                      # Constant: 1[kWh]
 
-#-------Solar Panels----------
-amps = 9         # Read from SolarAPI
-duration = 30    # Read from SolarAPI
+#----Accumulator
+acc_soc = 0.8                       # Read from TeslaAPI
+acc_soc_min = 0.2
+acc_capacity = 24.4
+acc_power = acc_soc * acc_capacity
+beta = 1
 
-beta_is_sunny = 1
-if amps >= 5:
-    beta_is_sunny = 1
-else: 
-    beta_is_sunny = 0
+if acc_soc < acc_soc_min:
+    acc_soc = acc_soc_min #TODO: why do I hardcode this?
 
-solar_power = beta_is_sunny * 220 * amps * duration * (1/1000)
+#----Solar Power
+amps = 9                                            # Read from SolarAPI
+duration = 5                                        # Read from SolarAPI
+solar_power = 230 * amps * duration * (1/1000)
 
-#-------Linear Program----------
-# Coefficients for x_i, respectively where i = 1..3 ==> x1 - (beta*220*a*d*x2) + powerwalls_soc*x3
-objective_function = [1, -solar_power, powerwall_power]
+#*************Linear Program Model*************
 
-lhs_ineq = [[0, 0, powerwall_power],                # Max. Powerwall Consumption [kwh]
-            [0, 0, powerwall_power],                # Min. Soc in Powerwall
-            [-1, solar_power, 0],                   # Einspeisen
-            [-1 , 0, -powerwall_power],   # Min. Supplied Total Power in [kwh]
-            [1, 0, powerwall_power]]      # Max. Supplied Total Power in [kwh]
+#----Coefficients 
+objective_function = [grid_power, -solar_power, beta * acc_power]
 
-rhs_ineq = [19.52,                            # Max. Powerwall Consumption [kwh]
-            -4.88+powerwall_power,            # Min. Soc in Powerwall
-            solar_power,                      # Einspeisen
-            -min_total_power_required + solar_power,        # Min. Supplied Total Power [kwh]
-            max_total_power_required - solar_power]         # Max. Supplied Total Power Worst Case [kwh] -- this no. will change based on the amount of power we need.
+#----Constraints
+lhs_ineq = [[0,   0,            acc_power ],                # (4.37) - Max. Accumulator Consumption [kwh]
+            [0,   0,            acc_power ],                # (4.38) - Min. Soc in Accumulator [kwh]
+            [-1,  solar_power,  0         ],                # (4.36) - Grid Export
+            [-1,  0,            -acc_power],                # (4.39) - Min. #Power demanded [kwh]
+            [1,   0,            acc_power ]]                # (4.40) - Max. #Power demanded [kwh] 
+
+rhs_ineq = [(1 - acc_soc_min) * acc_capacity,               # (4.37) - Max. Accumulator Consumption [kwh]
+            -(acc_soc_min * acc_capacity) + acc_power,      # (4.38) - Min. Soc in Accumulator [kwh]
+            solar_power,                                    # (4.36) - Grid Export
+            -min_total_power_required + solar_power,        # (4.39) - Min. #Power demanded [kwh]
+            max_total_power_required - solar_power]         # (4.40) - Max. #Power demanded [kwh] 
               
-
+#----Sign Restrictions
 bnd = [(float("-inf"), float("inf")),  # Bounds of x1 --> Grid
        (0, 1),                         # Bounds of X2 --> Solar
-       (0, 1)]                         # Bounds of x3 --> Powerwall
+       (0, 1)]                         # Bounds of x3 --> Accumulator
        
 opt = linprog(c=objective_function, 
               A_ub=lhs_ineq, 
@@ -65,31 +86,61 @@ opt = linprog(c=objective_function,
 
 print(opt)
 
-#-------Visualization----------
-objects = ('Grid', 'Solar', 'Powerwalls')
-x = np.arange(len(objects))
-y1_power_provider_usage = [opt.x[0], 
-     opt.x[1] * solar_power,
-     opt.x[2] * powerwall_power]
-y2_power_provider_capacity = [0, 
-               (solar_power - (opt.x[1] * solar_power)),
-               (powerwall_power - (opt.x[2] * powerwall_power))]
+#*************Visualization*************
+objects = ('Grid', 'Solar', 'Accumulators')
 
-plt.bar(x, y1_power_provider_usage, color=['red', 'green', 'blue'], align='center', alpha=0.8)
-plt.bar(x, y2_power_provider_capacity, color=['grey', 'grey', 'grey'], bottom=y1_power_provider_usage, alpha=0.5)
+#-----Configuration
+x = np.arange(len(objects))
+y1_power_producer_consumption = [opt.x[0], 
+     opt.x[1] * solar_power,
+     opt.x[2] * acc_power]
+y2_power_producer_capacity = [0, 
+               (solar_power - (opt.x[1] * solar_power)),
+               (acc_power   - (opt.x[2] * acc_power))]
+
+#-----Styling
+plt.bar(x, y1_power_producer_consumption, color=['red', 'green', 'blue'], align='center',                       alpha=0.8)
+plt.bar(x, y2_power_producer_capacity,    color=['grey', 'grey', 'grey'], bottom=y1_power_producer_consumption, alpha=0.5)
 plt.xticks(x, objects)
 plt.ylabel('Usage in [kwh]')
 plt.title('Power consumption allocation [kwh]')
 plt.grid(color='#95a5a6', linestyle='--', linewidth=2, axis='y', alpha=0.3)
 plt.show()
 
-print("Grid Pull: ", round(opt.x[0],2), "[kwh]")
-print("Solar Pull: ", round((opt.x[1] * solar_power),2), "[kwh] from", round(solar_power,2), "[kwh]")
-print("Powerwalls Pull: ", round((opt.x[2] * powerwall_power),2), "[kwh] from", round(powerwall_power,2), "[kwh]")
-total_pulled_power = (opt.x[0] + solar_power * (1 - opt.x[1]))+ (opt.x[1] * solar_power) + opt.x[2] * powerwall_power
-print("Total:", round(total_pulled_power,2), "[kwh]")
+#-----Logging
+print("Grid Pull: ",         round( opt.x[0], 2)               , "[kwh]")
+print("Solar Pull: ",        round((opt.x[1] * solar_power), 2), "[kwh] from", round(solar_power, 2), "[kwh]")
+print("Accumulators Pull: ", round((opt.x[2] * acc_power  ), 2), "[kwh] from", round(acc_power, 2), "[kwh]")
+
+print("----------------------")
+
+print("Accumulator Level Before Discharge:", acc_power, "[kwh]")
+print("Accumulator Level After Discharge:", round((acc_power - (opt.x[2] * acc_power)), 2), "[kwh]")
+print("Accumulator min. reserved power: ", acc_soc_min * acc_capacity, "[kwh]")
+
+print("----------------------")
+
+consumed_solar_power = (opt.x[1] * solar_power)
+consumed_acc_power = opt.x[2] * acc_power
+
+excess_solar_power = solar_power * (1 - opt.x[1])
+consumed_grid_power = opt.x[0]
+balanced_grid_power_meter = consumed_grid_power + excess_solar_power
+total_pulled_power = balanced_grid_power_meter + consumed_solar_power + consumed_acc_power
+print("Reserved power for Home:", avg_home_consumption, "[kwh]")
+print("Reserved power for Car:", round((total_pulled_power - avg_home_consumption), 2), "[kwh]")
+
+print("----------------------")
+
+print("Car SoC before:", (car_soc * 100), "%")
+print("Car SoC after:",  round((car_soc + (round(total_pulled_power - avg_home_consumption, 2) / car_battery_capacity)) * 100, 2), "%")
+
+print("----------------------")
+
+print("Total Power Reserved for Consumption:", round(total_pulled_power, 2), "[kwh]")
+
+print("----------------------")
+
 print("Optimized Coefficient Values:", opt.x)
-
-
 
 # %%
